c91f1455b1e84dc55135310d731242d0b0212462
hkduc2000 <36145851+hkduc2000@users.noreply.github.com>
2019-07-25 13:41:57 +0700

    first commit

---
+++ mysite/blog/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from blog.models import Post,Comment,tag_list
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        model = Post
+        fields = ('author', 'title', 'tag', 'text')
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class:':'textinputclass'}),
+            'tag': forms.Select(choices=tag_list),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent border border-primary',
+                                          'style':'min-height:300px;'}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comment
+        fields = ('author','text')
+
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class': 'editable medium-editor-textarea border'}),
+        }
